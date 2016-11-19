371f6533a7f786b289858e02764842a4a5934a97
Fabio Lourenzi <fabio.lourenzi@acad.pucrs.br>
2016-11-19 20:54:10 -0200

    #Removing unused tests

---
+++ monopoly/src/test/java/edu/nsu/monopoly/CellInfoFormatterTest.java
@@ -0,0 +1,46 @@
+package edu.nsu.monopoly;
+
+import edu.ncsu.monopoly.GoCell;
+import edu.ncsu.monopoly.Player;
+import edu.ncsu.monopoly.PropertyCell;
+import edu.ncsu.monopoly.gui.GoCellInfoFormatter;
+
+import edu.ncsu.monopoly.gui.InfoFormatter;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class CellInfoFormatterTest {
+    
+	@Test
+    public void testGoCellTest() {
+        GoCell cell = new GoCell();
+        String goLabel = GoCellInfoFormatter.GO_CELL_LABEL;
+        assertEquals(goLabel, InfoFormatter.cellInfo(cell));
+    }
+    
+	@Test
+    public void testPropertyCellText() {
+        String propertyName = "Blue 1";
+        String propertyColor = "blue";
+        String ownerName = "Owner1";
+        int numHouses = 2;
+        int propertyValue = 120;
+        String propertyLabel = "<html><b><font color='" +
+				propertyColor +"'>" + propertyName + "</font></b><br>" +
+				"$" + propertyValue +
+				"<br>Owner: " + ownerName +
+				"<br>* " + numHouses +
+				"</html>";
+        PropertyCell cell = new PropertyCell();
+        cell.setName(propertyName);
+        cell.setPrice(propertyValue);
+        cell.setColorGroup(propertyColor);
+        Player p = new Player();
+        p.setName(ownerName);
+        cell.setOwner(p);
+        cell.setNumHouses(numHouses);
+        assertEquals(propertyLabel, InfoFormatter.cellInfo(cell));
+    }
+}
